2a31036ca7c736e41236b99b4ffbf12953ad4510
Vedant1612 <113239505+Vedant1612@users.noreply.github.com>
2024-06-03 23:57:29 +0530

    fix all register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -20,7 +20,7 @@ const registerUser = asyncHandler( async (req, res) => {
     // return response
 
     const { fullName, password, email, username } = req.body
-    console.log("email : ", email);
+    // console.log("email : ", email);
 
     if(
         [fullName, email, password, username].some((field)=>
@@ -29,7 +29,7 @@ const registerUser = asyncHandler( async (req, res) => {
         throw new ApiError(400,"All fiels are required")
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ username }, { email }]
     })
 
@@ -38,17 +38,22 @@ const registerUser = asyncHandler( async (req, res) => {
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path
-    const coverImageLocalPath = req.files?.coverImage[0]?.path
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path
+
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
     if(!avatarLocalPath){
-        throw new ApiError(400,"Avatar file is required")
+        throw new ApiError(400,"Avatar file is required ...")
     }
 
     const avatar = await uploadOnCloudinary(avatarLocalPath)
     const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
     if(!avatar){
-        throw new ApiError(400,"Avatar file is required")
+        throw new ApiError(400,"Avatar file is required!!!")
     }
 
     const user = await User.create({
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -101,4 +101,4 @@ userSchema.methods.generateRefreshToken = function(){
 }
 
 
-export const User = Schema.model("User", userSchema);
+export const User = mongoose.model("User", userSchema);
