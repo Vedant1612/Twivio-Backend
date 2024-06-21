a2d7702a07e48e430adaaf0d9f649fde0cd46ad1
Vedant1612 <113239505+Vedant1612@users.noreply.github.com>
2024-06-21 16:57:08 +0530

    add sameSite to options for cookies

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -144,7 +144,8 @@ const loginUser = asyncHandler(async (req,res)=>{
 
     const options = {
         httpOnly: true,
-        secure: true
+        secure: true,
+        sameSite: "None"
     }
 
     return res
@@ -178,7 +179,8 @@ const logoutUser = asyncHandler(async (req,res) => {
 
     const options = {
         httpOnly: true,
-        secure: true
+        secure: true,
+        sameSite: "None"
     }
 
     return res
@@ -219,7 +221,8 @@ const refreshAccessToken = asyncHandler(async (req,res)=>{
     
         const options = {
             httpOnly: true,
-            secure: true
+            secure: true,
+            sameSite: "None"
         }
     
         const {accessToken, newRefreshToken} = await generateAccessAndRefreshTokens(user._id)
